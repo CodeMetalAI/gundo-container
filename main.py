f68d735e659196740bbe01dda0287a4d6aa9091e
Zander Brumbaugh <brumbzan@cs.washington.edu>
2024-02-17 13:02:45 -0800

    Custom capture

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import airsim
 import numpy as np
+from PIL import Image
 import os, time
+from multiprocessing import Process
 from control.gamepad import XboxController
+from datetime import datetime
 
 # connect to the AirSim simulator
 client = airsim.MultirotorClient()
@@ -13,21 +16,50 @@ client.armDisarm(True)
 client.takeoffAsync().join()
 # client.moveToPositionAsync(-10, 10, -10, 5).join()
 
+CAPTURE_MODE = True
+
 controller = XboxController()
 yaw_rate = np.pi
-max_roll = np.deg2rad(45)
-max_pitch = np.deg2rad(45)
+max_roll = np.deg2rad(75)
+max_pitch = np.deg2rad(75)
 input_rate = 0.01
 
-while True:
-    inputs = controller.read()
-    roll = inputs["rx"] * max_roll
-    pitch = inputs["ry"] * max_pitch
-    yaw = (inputs["yaw+"] - inputs["yaw-"]) * yaw_rate
-    throttle = float(np.clip(inputs["y"], 0, 1))
+capture_rate = 0.2
 
-    client.moveByRollPitchYawrateThrottleAsync(roll=roll, pitch=pitch, yaw_rate=yaw, throttle=throttle, duration=input_rate)
-    time.sleep(input_rate)
+def control_loop():
+    while True:
+        inputs = controller.read()
+        roll = inputs["rx"] * max_roll
+        pitch = inputs["ry"] * max_pitch
+        yaw = (inputs["yaw+"] - inputs["yaw-"]) * yaw_rate
+        throttle = float(np.clip(inputs["y"], 0, 1))
+
+        client.moveByRollPitchYawrateThrottleAsync(roll=roll, pitch=pitch, yaw_rate=yaw, throttle=throttle, duration=input_rate)
+        time.sleep(input_rate)
+
+def capture_loop():
+    now = datetime.now()
+    ts = now.strftime("%Y_%m_%d_%H_%M_%S_%f")[:-3]
+    os.mkdir(f"capture_data/{ts}")
+    i = 0
+
+    while True:
+        responses = client.simGetImages([airsim.ImageRequest("0", airsim.ImageType.Scene, False, False)])
+        response = responses[0]
+        img1d = np.frombuffer(response.image_data_uint8, dtype=np.uint8) 
+        img_rgb = img1d.reshape(response.height, response.width, 3)
+        img = Image.fromarray(img_rgb)
+        img.save(f"capture_data/{ts}/cap_{i}.png")
+        i += 1
+
+        time.sleep(capture_rate)
+
+if __name__ == '__main__':
+    p1 = Process(target=control_loop)
+    p1.start()
+
+    if CAPTURE_MODE:
+        capture_loop()
 
 # # take images
 # responses = client.simGetImages([
